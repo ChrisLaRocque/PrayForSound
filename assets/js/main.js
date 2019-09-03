df1c22156c4be02fd910f2eb2e511afcf896b068
Chris LaRocque <larocque.christopher@gmail.com>
2019-09-03 17:18:47 -0400

    Finishes out some sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 // HTML Loader
-var ids = $('.load').map(function(){
-  return $(this).attr('id');
+var ids = $(".load").map(function() {
+  return $(this).attr("id");
 });
 
 for (var i = 0; i < ids.length; i++) {
   const id = ids[i];
-  $.get( id + ".html", function(data){
-    $("#" + id ).replaceWith(data);
+  $.get(id + ".html", function(data) {
+    $("#" + id).replaceWith(data);
   });
 }
