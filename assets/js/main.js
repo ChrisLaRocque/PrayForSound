3553f47374f566af58df9a03cac64a9c688fe292
Chris LaRocque <larocque.christopher@gmail.com>
2019-09-18 20:24:59 -0400

    Adds GA/FB, up arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,21 @@ for (var i = 0; i < ids.length; i++) {
   });
 }
 
+// Top Button
+//Get the button:
+mybutton = document.getElementById("topBtnWrapper");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.opacity = "0";
+  }
+}
+
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $("a").on('click', function (event) {
